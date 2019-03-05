0563d4306ea46d670f2cedadbf87f0a59d4d3800
Geon Soo Park <geonsoo@uw.edu>
2019-03-05 12:23:43 -0800

    test

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 # Note: import the app of what you are working on
 # from initial_demo import app
-from exercise1 import app
-# from exercise2 import app
-# from exercise2 import app
+# from exercise1 import app
+
+from exercise2 import app
+
+# from vis_scatter_plot import app
+# from vis_line_chart import app
 
 server = app.server
 
